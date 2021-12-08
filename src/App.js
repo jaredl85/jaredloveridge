a544c4be493e3f8653752d0bcba17d622a4598a3
jaredl85 <jaredloveridge@gmail.com>
2021-12-08 11:07:20 -0800

    added scroll-to-top functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import NeChiro from "./components/NeChiro.js";
 import Portfolio from "./components/Portfolio.js";
@@ -12,18 +12,29 @@ import ScrollToTop from './ScrollToTop';
 function App() {
   return (
     <div className="app">
-      <ScrollToTop>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/nechiro" element={<NeChiro />} />
-            <Route path="/dogwithin" element={<DogWithin />} />
-            <Route path="/nucamp" element={<NuCamp />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </Router>
-      </ScrollToTop>
+      <Router>
+        <ScrollToTop />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/nechiro">
+              <NeChiro />
+            </Route>
+            <Route path="/dogwithin">
+              <DogWithin />
+            </Route>
+            <Route path="/nucamp">
+              <NuCamp />
+            </Route>
+            <Route path="/portfolio">
+              <Portfolio />
+            </Route>
+            <Route path="*">
+              <Error />
+            </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
